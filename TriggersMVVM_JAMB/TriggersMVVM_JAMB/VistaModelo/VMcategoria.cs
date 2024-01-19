3aa599fa26cf1046e8049d9440e923a626af0535
DonAntonio04 <berrellezantonio04@gmail.com>
2024-01-18 19:15:40 -0700

    segundo

--- TriggersMVVM_JAMB/TriggersMVVM_JAMB/VistaModelo/VMcategoria.cs
+++ TriggersMVVM_JAMB/TriggersMVVM_JAMB/VistaModelo/VMcategoria.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using TriggersMVVM_JAMB.Modelo;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace TriggersMVVM_JAMB.VistaModelo
 {
@@ -13,6 +15,8 @@ namespace TriggersMVVM_JAMB.VistaModelo
     {
         #region VARIABLES
         string _Texto;
+        bool _activadorAnimacionImg;
+        string _imagen;
         ObservableCollection<Mcategorias> _listaCategorias;
         #endregion
         #region CONSTRUCTOR
@@ -28,9 +32,44 @@ namespace TriggersMVVM_JAMB.VistaModelo
             get { return _listaCategorias; }
             set { SetValue(ref _listaCategorias, value); }
         }
+        public bool ActivadorAnimacionImg
+        {
+            get { return _activadorAnimacionImg; }
+            set { SetValue(ref _activadorAnimacionImg, value); }
+        }
+        public string Imagen
+        {
+            get { return _imagen; }
+            set { SetValue(ref _imagen, value); }
+        }
         #endregion
         #region PROCESOS
-        public async Task ProcesoAsyncrono()
+        public void Seleccionar(Mcategorias modelo)
+        {
+            var index = ListaCategorias.ToList().FindIndex(p => p.descripcion == modelo.descripcion);
+
+            Imagen = modelo.imagen;
+            if (index > -1)
+            {
+                Deseleccionar();
+                ActivadorAnimacionImg = true;
+                ListaCategorias[index].selected = true;
+                ListaCategorias[index].textColor = "#FFFFFF";
+                ListaCategorias[index].backgroundColor = "#FF506B";
+            }
+        }
+        public void Deseleccionar()
+        {
+            ListaCategorias.ForEach((item) =>
+            {
+                ActivadorAnimacionImg = false;
+                item.selected = false;
+                item.textColor = "#000";
+                item.backgroundColor = "#EAEDF6";
+            });
+        }
+
+            public async Task ProcesoAsyncrono()
         {
 
         }
@@ -39,10 +78,13 @@ namespace TriggersMVVM_JAMB.VistaModelo
             //Al hacer esto estamos jalando toda la data
             ListaCategorias = new ObservableCollection<Mcategorias>(Datos.Dcategorias.MostrarCategorias());
         }
+
         #endregion
         #region COMANDOS
         public ICommand ProsesoAsynCommnad => new Command(async () => await ProcesoAsyncrono());
         public ICommand MostrarCategoriasCommnad => new Command(MostrarCategorias);
+        public ICommand SeleccionarCommand => new Command<Mcategorias>((p) => Seleccionar(p));
+
         #endregion
     }
 }
--- TriggersMVVM_JAMB/TriggersMVVM_JAMB/obj/Debug/netstandard2.0/Vistas/PaginaCategoria.xaml.g.cs
+++ TriggersMVVM_JAMB/TriggersMVVM_JAMB/obj/Debug/netstandard2.0/Vistas/PaginaCategoria.xaml.g.cs
@@ -17,8 +17,12 @@ namespace TriggersMVVM_JAMB.Vistas {
     public partial class PaginaCategoria : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.CollectionView listaCategorias;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(PaginaCategoria));
+            listaCategorias = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CollectionView>(this, "listaCategorias");
         }
     }
 }
